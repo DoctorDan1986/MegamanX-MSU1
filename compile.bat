7f53e00f06822414ed9995e1ed76253ac1d38024
Michaël Larouche <michael.larouche@gmail.com>
2014-11-13 15:09:50 -0500

    * More music * Track looping/not looping * Integrate MSU_Main where PlayMusicSPC is called in the game

--- compile.bat
+++ compile.bat
@@ -1,5 +1,24 @@
 @ECHO OFF
 
 del mmx_msu1.sfc
+del *.pcm
+
 copy mmx_original.sfc mmx_msu1.sfc
 asar mmx_msu1_music.asm mmx_msu1.sfc
+
+rem mmx_msu1-16.wav selectionStart=126976 selectionEnd=813056
+wav2msu mmx_msu1_intro_stage.wav mmx_msu1-1.pcm
+wav2msu mmx_msu1_sting_chameleon.wav mmx_msu1-2.pcm
+wav2msu mmx_msu1_chill_penguin.wav mmx_msu1-7.pcm
+wav2msu mmx_msu1_storm_eagle.wav mmx_msu1-9.pcm
+wav2msu mmx_msu1_boss_battle_1.wav mmx_msu1-14.pcm
+wav2msu mmx_msu1_title_screen.wav mmx_msu1-15.pcm
+wav2msu mmx_msu1-16.wav -l 126976
+wav2msu mmx_msu1_boss_victory_jingle.wav mmx_msu1-17.pcm
+wav2msu mmx_msu1_stage_selected_jingle.wav mmx_msu1-18.pcm
+wav2msu mmx_msu1_got_a_weapon.wav mmx_msu1-23.pcm
+wav2msu mmx_msu1_password.wav mmx_msu1-24.pcm
+wav2msu mmx_msu1_ending_theme.wav mmx_msu1-28.pcm
+wav2msu mmx_msu1_cast_roll.wav mmx_msu1-29.pcm
+wav2msu mmx_msu1_boss_tension_1.wav mmx_msu1-30.pcm
+
--- mmx_msu1_music.asm
+++ mmx_msu1_music.asm
@@ -17,13 +17,30 @@ MSU_STATUS_DATA_BUSY     = %1000000
 org $8692B8
 	db "MSU1 Hack by DarkSho"
 
-; This is the play music routine
-; A = Music to play + $10
+; Play Capcom Logo
+org $808613
+	jsr MSU_Main
+
+; Play music from Options Screen, All music after level music, Password Screen, Stage Select
 org $8087AA
-	jsr CheckMSU
+	jsr MSU_Main
+
+; Play Title Screen
+org $808D8F
+	jsr MSU_Main
 	
+; Play music at level load
+org $809A2D
+	jsr MSU_Main
+
+; Play stage selected jingle
+org $809709
+	jsr MSU_Main
+
+; A = Music to play + $10
 org $80FBCE
-CheckMSU:
+MSU_Main:
+	php
 ; Backup A and Y in 16bit mode
 	rep #$30
 	pha
@@ -39,13 +56,14 @@ CheckMSU:
 	
 MSUFound:
 	; Set volume
-	lda #$FF
+	lda #$BE
 	sta !MSU_AUDIO_VOLUME
 	
 	; Set track
 	tya
 	clc
 	sbc #$F
+	tay
 	sta MSU_AUDIO_TRACK_LO
 	stz !MSU_AUDIO_TRACK_HI
 
@@ -59,19 +77,46 @@ MSUFound:
 	and.b #MSU_STATUS_AUDIO_BUSY
 	bne .CheckAudioStatus
 	
-	; Play the song and repeat it
-	lda #$01
+	; Play the song and add repeat if needed
+	jsr TrackNeedLooping
 	sta !MSU_AUDIO_CONTROL
 
 	rep #$30
 	ply
 	pla
+	plp
 	rts
 	
 MSUNotFound:
 	rep #$30
 	ply
 	pla
+	plp
 	
 	jsr $87B0
+	rts
+	
+TrackNeedLooping:
+; Capcom Jingle
+	cpy #$00
+	beq .noLooping
+; Title Screen
+	cpy #$0F
+	beq .noLooping
+; Victory Jingle 
+	cpy #$11
+	beq .noLooping
+; Stage Selected Jingle
+	cpy #$12
+	beq .noLooping
+; Got a Weapon 
+	cpy #$17
+	beq .noLooping
+; Boss Tension 1
+	cpy #$1E
+	beq .noLooping
+	lda #$03
+	rts
+.noLooping:
+	lda #$01
 	rts
