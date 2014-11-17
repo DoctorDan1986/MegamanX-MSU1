308f34d4e4dfbf9868c4cc643f9a6067332f58c4
Michaël Larouche <michael.larouche@gmail.com>
2014-11-17 12:55:46 -0500

    * Add support for stop and resume music command. Still no fade-in/fade-out

--- mmx_msu1_music.asm
+++ mmx_msu1_music.asm
@@ -6,20 +6,25 @@ MSU_AUDIO_TRACK_LO = $2004
 !MSU_AUDIO_TRACK_HI = $2005
 !MSU_AUDIO_VOLUME = $2006
 !MSU_AUDIO_CONTROL = $2007
+SPC_PORT_0 = $2140
 
 ; MSU_STATUS possible values
 MSU_STATUS_TRACK_MISSING = $8
-MSU_STATUS_AUDIO_PLAYING = %0001000
-MSU_STATUS_AUDIO_REPEAT  = %0010000
+MSU_STATUS_AUDIO_PLAYING = %00010000
+MSU_STATUS_AUDIO_REPEAT  = %00100000
 MSU_STATUS_AUDIO_BUSY    = $40
-MSU_STATUS_DATA_BUSY     = %1000000
+MSU_STATUS_DATA_BUSY     = %10000000
 
 org $8692B8
 	db "MSU1 Hack by DarkSho"
 
+; Add a hook to where the sound effects/special commands are played
+org $80885B
+	jsr MSU_SoundEffectsAndCommand
+
 ; Play Capcom Logo
-;org $808613
-;	jsr MSU_Main
+org $808613
+	jsr MSU_Main
 
 ; Play music from Options Screen, All music after level music, Password Screen, Stage Select
 org $8087AA
@@ -35,6 +40,10 @@ org $809A2D
 
 ; Play stage selected jingle
 org $809709
+	jsr MSU_Main
+	
+; Ending ??
+org $809CFA
 	jsr MSU_Main
 
 ; A = Music to play + $10
@@ -52,9 +61,9 @@ MSU_Main:
 	; Check if MSU is present
 	lda MSU_ID
 	cmp #'S'
-	bne MSUNotFound
+	bne .MSUNotFound
 	
-MSUFound:
+.MSUFound:
 	; Set volume
 	lda #$60
 	sta !MSU_AUDIO_VOLUME
@@ -68,8 +77,8 @@ MSUFound:
 	stz !MSU_AUDIO_TRACK_HI
 
 	lda MSU_STATUS
-	and.b #$8
-	bne MSUNotFound
+	and.b #MSU_STATUS_TRACK_MISSING
+	bne .MSUNotFound
 	
 .CheckAudioStatus
 	lda MSU_STATUS
@@ -87,7 +96,7 @@ MSUFound:
 	plp
 	rts
 	
-MSUNotFound:
+.MSUNotFound:
 	rep #$30
 	ply
 	pla
@@ -119,4 +128,53 @@ TrackNeedLooping:
 	rts
 .noLooping:
 	lda #$01
+	rts
+	
+MSU_SoundEffectsAndCommand:
+	php
+	
+	sep #$30
+	pha
+	
+	lda MSU_ID
+	cmp #'S'
+	bne .MSUNotFound_SE
+	
+	pla
+	; $F5 is a command to resume music
+	cmp #$F5
+	beq .ResumeMusic
+	; $F6 is a command to fade-out music (currently stopping it)
+	cmp #$F6
+	beq .StopMusic
+	; If not, play the sound as the game excepts to
+	bra .PlaySound
+	
+.ResumeMusic:
+	lda #$F6
+	sta SPC_PORT_0
+	
+	; TODO: Fade-in ?
+	lda #$03
+	sta !MSU_AUDIO_CONTROL
+	bra .CleanupAndReturn
+
+.StopMusic:
+	sta SPC_PORT_0
+
+	lda MSU_STATUS
+	and.b #MSU_STATUS_AUDIO_PLAYING
+	beq .CleanupAndReturn
+
+	; Stop current music (TODO: Fade-out)
+	lda #$00
+	sta !MSU_AUDIO_CONTROL
+	bra .CleanupAndReturn
+
+.MSUNotFound_SE:
+	pla
+.PlaySound:
+	sta SPC_PORT_0
+.CleanupAndReturn:
+	plp
 	rts
